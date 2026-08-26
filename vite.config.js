5a0556273abc0fb42800470fd2e8ac483c667808
Yaz <ayaz.toronto@gmail.com>
2026-08-26 12:01:30 -0400

    Adding icons for app

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Today's Tasks",
+        short_name: "Tasks",
+        description: "A simple daily task manager",
+
+        theme_color: "#28282c",
+        background_color: "#28282c",
+
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     allowedHosts: ["return-perfect-kids-shift.trycloudflare.com"],
   },
